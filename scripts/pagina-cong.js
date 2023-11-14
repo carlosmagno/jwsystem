f3780da8f84232ffe16fb4e984f106ac01c12b8d
Carlos Magno <carlosmagno.tjsa@gmail.com>
2023-11-14 18:25:03 -0300

    Ajustes

--- scripts/pagina-cong.js
+++ scripts/pagina-cong.js
@@ -19,9 +19,30 @@ function exibeAreaAdmin(){
           console.log(snapshot.val());
             snapshot.array.forEach(element => {
                 console.log(element)
+
+                
+                var divNova = document.createElement("div");
+                var conteudoNovo = document.createTextNode(element);
+                var perfil = document.createElement('select')
+                perfil.setAttribute('id', 'selectPerfilUser')
+                perfil.innerHTML ='<option value="Leitor">Leitor</option><option value="Editor">Editor</option>'
+                divNova.appendChild(conteudoNovo);
+                divNova.appendChild(perfil); //adiciona o nó de texto à nova div criada
+
+                // adiciona o novo elemento criado e seu conteúdo ao DOM
+                var pAtual = document.getElementById("pTitulo");
+                pAtual.insertAdjacentElement('afterend', divAtual);
+                
             });
         } else {
-          console.log("Não há dados neste nó");
+          console.log("Não há dados neste nó, aguarde ...");
+          var usuarioAdmin ={
+            email,
+            displayName,
+            perfil:"Admin"
+          }
+          refUsuarios.child(uid).set(usuarioAdmin)
+          console.log(`Usuário admin cadastrado: ${displayName}`);
         }
     
     })
