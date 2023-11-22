baa77e96340eff48e0f3d5cf605b4b3d4f2d6017
Carlos Magno <carlosmagno.tjsa@gmail.com>
2023-11-21 21:09:31 -0300

    Update pagina-cong.js

--- scripts/pagina-cong.js
+++ scripts/pagina-cong.js
@@ -23,18 +23,8 @@ var userEmAlteracao;
 function exibeAreaAdmin(){
     var areaAdmin = document.getElementById('areaAdmin')
     var dadosBDAdmin = document.getElementById("dadosBDAdmin");
-    var spanNomeUsuario = document.getElementById('spanNomeUsuario');
-    
-    /**
-     *Elementos dos checkboxes
-     */
+  
 
-     var chkRegPublicadores = document.getElementById('chkRegPublicadores');
-     var chkConPublicadores = document.getElementById('chkConPublicadores');
-     var chkRegRelatorios = document.getElementById('chkRegRelatorios');
-     var chkConRelatorios = document.getElementById('chkConRelatorios');
-     var chkRegReunioes = document.getElementById('chkRegReunioes');
-     var chkConReunioes = document.getElementById('chkConReunioes');
 
 
     areaAdmin.setAttribute('class', 'aparente')
@@ -45,6 +35,7 @@ function exibeAreaAdmin(){
 
           var perfilUpdate = document.createElement('select')
           perfilUpdate.setAttribute('class', 'selectDinamica')
+          perfilUpdate.setAttribute( 'id','selectUsers')
           var opcao2 = document.createElement('option')
           opcao2.setAttribute("label", "")
           perfilUpdate.appendChild(opcao2)
@@ -57,62 +48,14 @@ function exibeAreaAdmin(){
 
                   var conteudoNovo = document.createTextNode(ValorNo.email);
                   var opcao = document.createElement('option')
-                  var perfilExistente
+                  
 
                   opcao.setAttribute("label", ValorNo.email)
                   opcao.setAttribute('value', chave)
                   opcao.appendChild(conteudoNovo)
                   perfilUpdate.appendChild(opcao)
                   
-                  perfilUpdate.onchange=function(){
-                    //console.log('evt.target: ', evt.target) 
-                    //console.log("value: ", this.value)
-                    //console.log("label: ", this.label)
-                    userEmAlteracao = this.value
 
-                    chkRegPublicadores.removeAttribute("checked")
-                    chkConPublicadores.removeAttribute("checked")
-                    chkRegRelatorios.removeAttribute("checked")
-                    chkConRelatorios.removeAttribute("checked")
-                    chkRegReunioes.removeAttribute("checked")
-                    chkConReunioes.removeAttribute("checked")
-
-                    refUsuarios.child(userEmAlteracao).child('perfil').get().then((snapshot)=>{
-                      perfilExistente =  snapshot.val()
-                      
-                      spanNomeUsuario.innerText=""
-                      spanNomeUsuario.innerText = this.value;
-                      console.log("perfil baixado do BD")
-
-                      
-                    if(perfilExistente){
-                      console.log('perfil existente: ', perfilExistente )
-                      if(perfilExistente.chkRegPublicadores=="on"){chkRegPublicadores.setAttribute("checked","checked")}
-                      else{chkRegPublicadores.removeAttribute("checked")};
-
-                      if(perfilExistente.chkConPublicadores=="on") {chkConPublicadores.setAttribute("checked","checked")}
-                      else{chkConPublicadores.removeAttribute("checked")};
-
-                      if(perfilExistente.chkRegRelatorios=="on") {chkRegRelatorios.setAttribute("checked","checked")}
-                      else{chkRegRelatorios.removeAttribute("checked")};
-
-                      if(perfilExistente.chkConRelatorios=="on") {chkConRelatorios.setAttribute("checked","checked")}
-                      else{chkConRelatorios.removeAttribute("checked")}; 
-
-                      if(perfilExistente.chkRegReunioes=="on") {chkRegReunioes.setAttribute("checked","checked")}
-                      else{chkRegReunioes.removeAttribute("checked")};
-
-                      if(perfilExistente.chkConReunioes=="on") {chkConReunioes.setAttribute("checked","checked")}
-                      else{chkConReunioes.removeAttribute("checked")};
-
-                    }else{
-                      console.log('ainda não existe perfil')
-                    }
-
-                    });
-                    
-
-                  }
             });
 
             dadosBDAdmin.appendChild(perfilUpdate);
@@ -135,16 +78,22 @@ function exibeAreaAdmin(){
 
 }
 
+function atualizaUsuario(){
+
+}
+
 function analisaCheckboxes(checK){
   var checK1 = document.getElementById(`${checK}`);
   var valor = checK1.getAttribute('checked')
 
   if(valor=="" ||valor=="checked"){
-    valor = "on"
     checK1.setAttribute('checked','checked')
+    valor = "on"
+    
   }else if(valor==null){
-    valor = "off"
+
     checK1.removeAttribute('checked')
+    valor = "off"
   }
   console.log(valor)
   return valor
@@ -202,3 +151,66 @@ function fechaConsultaGrupo(){
   var ConsultaGrupo = document.getElementById('ConsultaGrupo')
   ConsultaGrupo.setAttribute("class","oculto")
 };
+
+if(document.getElementById('selectUsers')){
+  var selectUsers = document.getElementById('selectUsers')
+}
+
+selectUsers.onchange=function(){
+
+  var chkRegPublicadores = document.getElementById('chkRegPublicadores');
+  var chkConPublicadores = document.getElementById('chkConPublicadores');
+  var chkRegRelatorios = document.getElementById('chkRegRelatorios');
+  var chkConRelatorios = document.getElementById('chkConRelatorios');
+  var chkRegReunioes = document.getElementById('chkRegReunioes');
+  var chkConReunioes = document.getElementById('chkConReunioes');
+  var spanNomeUsuario = document.getElementById('spanNomeUsuario');
+
+  //console.log('evt.target: ', evt.target) 
+  //console.log("value: ", this.value)
+  //console.log("label: ", this.label)
+  userEmAlteracao = this.value
+
+  chkRegPublicadores.removeAttribute("checked")
+  chkConPublicadores.removeAttribute("checked")
+  chkRegRelatorios.removeAttribute("checked")
+  chkConRelatorios.removeAttribute("checked")
+  chkRegReunioes.removeAttribute("checked")
+  chkConReunioes.removeAttribute("checked")
+
+  refUsuarios.child(userEmAlteracao).child('perfil').get().then((snapshot)=>{
+    var perfilExistente =  snapshot.val()
+    
+    spanNomeUsuario.innerText=""
+    spanNomeUsuario.innerText = this.value;
+    console.log("perfil baixado do BD")
+
+    
+  if(perfilExistente){
+    console.log('perfil existente: ', perfilExistente )
+    if(perfilExistente.chkRegPublicadores=="on"){chkRegPublicadores.setAttribute("checked","checked")}
+    else{chkRegPublicadores.removeAttribute("checked")};
+
+    if(perfilExistente.chkConPublicadores=="on") {chkConPublicadores.setAttribute("checked","checked")}
+    else{chkConPublicadores.removeAttribute("checked")};
+
+    if(perfilExistente.chkRegRelatorios=="on") {chkRegRelatorios.setAttribute("checked","checked")}
+    else{chkRegRelatorios.removeAttribute("checked")};
+
+    if(perfilExistente.chkConRelatorios=="on") {chkConRelatorios.setAttribute("checked","checked")}
+    else{chkConRelatorios.removeAttribute("checked")}; 
+
+    if(perfilExistente.chkRegReunioes=="on") {chkRegReunioes.setAttribute("checked","checked")}
+    else{chkRegReunioes.removeAttribute("checked")};
+
+    if(perfilExistente.chkConReunioes=="on") {chkConReunioes.setAttribute("checked","checked")}
+    else{chkConReunioes.removeAttribute("checked")};
+
+  }else{
+    console.log('ainda não existe perfil')
+  }
+
+  });
+  
+
+}
